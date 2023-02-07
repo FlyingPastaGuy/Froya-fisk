3537fcefbf105e29e038a6a0f9da5a573af82afb
millan-singh-sidhu <73245079+millan-singh-sidhu@users.noreply.github.com>
2023-02-07 12:01:10 +0100

    nav style and picture

--- froya/src/components/shared/NorNavbar.vue
+++ froya/src/components/shared/NorNavbar.vue
@@ -1,23 +1,49 @@
 <template>
+<div class="back">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <router-link to="/" class="Links">
+                <p class="nav-link text-light">hjem</p>
+            </router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/Kontakt-oss" class="Links">
+                <p class="nav-link text-light">Kontakt oss</p>
+            </router-link>        
+        </li>
+        <li class="nav-item">
+            <router-link to="/Om-oss" class="Links">
+                <p class="nav-link text-light">Om-oss</p>
+            </router-link>
+        </li>
+        <li>
+            <img src="../../assets/logo/Froya_logo_hvit.png" alt="" srcset="">
+        </li>
+        <li class="nav-item">
+            <router-link to="/Produkter" class="Links">
+                <p class="nav-link text-light">Produkter</p>
+            </router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/Oppskrifter" class="Links">
+                <p class="nav-link text-light">Oppskrifter</p>
+            </router-link>
+        </li>
+        <li class="nav-item">
 
-    <router-link to="/">
-        <p>hjem</p>
-    </router-link>
+            <p class="nav-link text-light">handlekurv</p>
 
-    <router-link to="/Kontakt-oss">
-        <p>Kontakt oss</p>
-    </router-link>
-
-    <router-link to="/Om-oss">
-        <p>Om-oss</p>
-    </router-link>
-
-    <router-link to="/Produkter">
-        <p>Produkter</p>
-    </router-link>
-
-    <router-link to="/Oppskrifter">
-        <p>Oppskrifter</p>
-    </router-link>
-
+        </li>
+    </ul>
+</div>
 </template>
+
+<style scoped>
+.back{
+    background-color: #151517;
+}
+
+.Links{
+    text-decoration:none
+}
+</style>
