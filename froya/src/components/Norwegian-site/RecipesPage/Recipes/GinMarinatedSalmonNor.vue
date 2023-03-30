f2a380249a13a4251803c66f10fe88f9bad5593c
Millan Singh Sidhu <73245079+millan-singh-sidhu@users.noreply.github.com>
2023-03-30 17:43:12 +0200

    Recipes/ retter er nå klare for styling

---
+++ froya/src/components/Norwegian-site/RecipesPage/Recipes/GinMarinatedSalmonNor.vue
@@ -0,0 +1,126 @@
+<template>
+<nor-navbar></nor-navbar>
+<div class="bg-white">
+    <article class="container py-5 my-5">
+        <h2 class="text-black">Ginmarinert Frøya laks</h2>
+        <p>Marinert laks med agruk, gin og rips</p>
+            <div class="row">
+                <div class="col">
+                    <h6>ingredienser</h6>
+                    <p>Til 4 personer trenger du:</p>
+                    <ul>
+                        <li>
+                            <p>70 gram sukker</p>
+                        </li>
+                        <li>
+                            <p>70 gram salt</p>
+                        </li>
+                        <li>
+                            <p>1 ts revet sitronskall</p>
+                        </li>
+                        <li>
+                            <p>1/2 ts hel sort pepper kunst i morter</p>
+                        </li>
+                        <li>
+                            <p>1 ts enebær knust i morter</p>
+                        </li>
+                        <li>
+                            <p>1/2 ts rosepepper knust i morter</p>
+                        </li>
+                        <li>
+                            <p>70 ml gin (klassisk gin)</p>
+                        </li>
+                        <li>
+                            <p>300 gram Frøya backloin </p>
+                        </li>
+                        <li>
+                            <p> 1 agruk</p>
+                        </li>
+                    </ul>
+                    <p>
+                        Til servering Rips, mynteblader og 
+                        babyleaf salat (finn en som også har 
+                        noen røde blader), olivenolje
+                    </p>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <div class="col-3">45 min</div>
+                        <div class="col-3">mediun</div>
+                        <div class="col-3"> 8 people</div>
+                    </div>
+                    <img class="img-thumbnail" src="@/assets/Images/Froya-til-fest-forrett-med-gin-marinert-froyalaks.jpg" alt="">
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col">
+                    <h6>Fremgangsmåte</h6>
+                    <p>
+                        Bland alle krydderne, salt og sukker med
+                        gin. Legg halvparten av blandingen i en
+                        glassform. Legg fisken oppå og ta deretter
+                        resten av blandingen på fisken. Dekk formen
+                        godt med plast og sett det i kjøleskapet 
+                        over natten. Når du skal tilbrede fisken til
+                        servering
+                    </p>
+                </div>
+                <div class="col">
+                    <ol>
+                        <li>
+                            <p> 
+                            skyll fisken, men bevar marinaden. Tørk fisken med et 
+                            kjøkkenpapir
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Høvle agurk med en ostehøvel legg på kjøkkenpapir slik 
+                                at fukten trekkes opp 
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Skjær fisken tynt i lengder
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Legg den opp på tallerken med rips, agurk salat, rise 
+                                olje over. Ta ett drag med pepperkvern over før 
+                                servering
+                            </p>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <router-link to="/Oppskrifter">
+                <input type="button" class="btn" value="Alle oppskrifter">
+            </router-link>
+    </article>
+</div>
+<nor-footer></nor-footer>
+</template>
+
+
+<script>
+import NorFooter from '@/components/shared/NorFooter.vue'
+import NorNavbar from '@/components/shared/NorNavbar.vue'
+
+export default {
+    setup() {
+        
+    },components: {
+        NorFooter,
+        NorNavbar
+    }
+}
+</script>
+
+
+<style scoped>
+p{
+    color: black;
+}
+
+</style>
---
+++ froya/src/components/Norwegian-site/RecipesPage/Recipes/SalmonAndChampagneBeurreBlancNor.vue
@@ -0,0 +1,236 @@
+<template>
+<nor-navbar></nor-navbar>
+<div class="bg-white">
+    <article class="container py-5 my-5">
+        <h2 class="text-black">Laks og champgne beurre blanc</h2>
+        <p>
+            Pannestekt laks med champgne beurre blanc, aspargesbønner og hasselback
+            poteter
+        </p>
+            <div class="row">
+                <div class="col">
+                    <h6>MID LOIN</h6>
+                    <p>Til 4 personer trenger du: 800 gram Frøya midloin</p>
+                    <h6>aspargesbønner</h6>
+                    <ul>
+                        <li>
+                            <p>2 pakker aspargesbønner</p>
+                        </li>
+                        <li>
+                            <p>2 ss Olivenolje</p>
+                        </li>
+                        <li>
+                            <p>1 sitron</p>
+                        </li>
+                    </ul>
+                    <h6>Saus: Beurre Blanc med champgne</h6>
+                    <ul>
+                        <li>
+                            <p>50 gram smør</p>
+                        </li>
+                        <li>
+                            <p>2 sjalottløk finhakket</p>
+                        </li>
+                        <li>
+                            <p>4 dl tørr champgne</p>
+                        </li>
+                        <li>
+                            <p>3 dl god fiskekraft</p>
+                        </li>
+                        <li>
+                            <p>5 dl fløte</p>
+                        </li>
+                    </ul>
+                    <h6>Hasselback poteter</h6>
+                    <ul>
+                        <li>
+                            <p>
+                                12 medium store poteter feks 
+                                Amadine
+                            </p>
+                        </li>
+                        <li>
+                            <p>6 ss smør smeltet</p>
+                        </li>
+                        <li>
+                            <p>1/2 ts paprika</p>
+                        </li>
+                        <li>
+                            <p>salt og pepper</p> 
+                        </li>
+                    </ul>
+                    <h6>Til servering</h6>
+                    <ul>
+                        <li>
+                            <p>laks eller ørretrogn</p>
+                        </li>
+                        <li>
+                            <p>1 sitron</p>
+                        </li>
+                        <li>
+                            <p>Krydderurter</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-3">20 min</div>
+                        <div class="col-3">mediun</div>
+                        <div class="col-3">4 people</div>
+                    </div>
+                    <img class="img-thumbnail" src="@/assets/Images/Froya-til-fest-forrett-med-gin-marinert-froyalaks.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col">
+                    <h6>Fremgangsmåte</h6>
+                    <p>Saus: Beurre Blanc med champgne</p>
+                    <ol>
+                        <li>
+                            <p> 
+                            Sett platen på medium varme. Smelt halvparten av
+                            smøret, tilsett sjalottløken som du svitsjer i smøret.
+                            Løken skal bare synke litt det skal ikke brunes så 
+                            pass på å stå ved gryten.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Tilsett champagnen og la det koke på medium
+                                varme til halvparten er igjen
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Tilsett fiskekraft og kok til dette er redusert til
+                                halv mengde
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Tilsett fløte og la det koke forsiktig til du har en 
+                                jevn saus
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Sil sausen gjennom en finmasket sil og visp inn
+                                resten av smøret
+                            </p>
+                        </li>
+                        <li>
+                            <p>Sett til side</p>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col">
+                    <h6>Hasselback poteter</h6>
+                    <ol>
+                        <li>
+                            <p>
+                                Sett stekeovnen på 225 grader, Finn fram
+                                et stekebrett eller en ildfast form. Skrell 
+                                potetene og skjær av bunnen slik at de
+                                står helt flatt.
+                            </p>    
+                        </li>
+                        <li>
+                            <p>
+                                Smelte smøret og smør formen.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Skjær tynne skiver ned i poteten, men ikke
+                                igjennom, se bildet.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Legg potetene i formen, pensle godt med
+                                smør, krydre med salt, pepper og paprika
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Stek potetene i midten av ovnen 45-60
+                                minutter avhengig av ovnen. Potetene skal
+                                ha et vakkert mørkt gyllent uttrykk
+                            </p>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col">
+                    <h6>Aspargesbønner</h6>
+                    <ol>
+                        <li>
+                            <p>
+                                Trim bønnene. kok dem raskt i 2min. skyll
+                                dem i kaldt vann slik at de holder fargen.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Når du serverer risler du olje og sitron over
+                                bønnene
+                            </p>
+                        </li>
+                    </ol>
+                    <h6>Frøya laks</h6>
+                    <ol>
+                        <li>
+                            <p>skjær laksen i porsjonstykker</p>
+                        </li>
+                        <li>
+                            <p>
+                                Stek den på middels høy varme i 2-3
+                                minutter på hver side. Fisken skal ha en rosa
+                                kjerne.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Salt og pepper
+                            </p>
+                        </li>
+                    </ol>
+                    <p>
+                        Servering: Når alt er ferdig legger du på en
+                        varmet tallerken opp fisk, poteter og bønner.
+                        sausen varmes forsiktig opp før servering.
+                        dandere på rognen.
+                    </p>
+                </div>
+            </div>
+            <router-link to="/Oppskrifter">
+                <input type="button" class="btn" value="Alle oppskrifter">
+            </router-link>
+    </article>
+</div>
+<nor-footer></nor-footer>
+</template>
+
+
+<script>
+import NorFooter from '@/components/shared/NorFooter.vue'
+import NorNavbar from '@/components/shared/NorNavbar.vue'
+
+export default {
+    setup() {
+        
+    },components: {
+        NorFooter,
+        NorNavbar
+    }
+}
+</script>
+
+
+<style scoped>
+p{
+    color: black;
+}
+
+</style>
