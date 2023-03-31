01ba7661dfd6d98222eb1e7d33a1e59c1b2b5e24
Millan Singh Sidhu <73245079+millan-singh-sidhu@users.noreply.github.com>
2023-03-31 14:22:04 +0200

    Update AboutUsNorView.vue

--- froya/src/views/Norwegian-site/AboutUsNorView.vue
+++ froya/src/views/Norwegian-site/AboutUsNorView.vue
@@ -1,3 +1,17 @@
 <template>
+<main-nor-navbar></main-nor-navbar>
     <h1> Om oss</h1>
+<nor-footer></nor-footer>
 </template>
+
+
+<script>
+import MainNorNavbar from '@/components/shared/MainNorNavbar.vue'
+import NorFooter from '@/components/shared/NorFooter.vue'
+export default {
+  components: { MainNorNavbar, NorFooter },
+    setup() {
+        
+    },
+}
+</script>
