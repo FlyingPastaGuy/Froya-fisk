2bc7b625bc7baaa4797aad18b14eaa066480d851
Millan Singh Sidhu <73245079+millan-singh-sidhu@users.noreply.github.com>
2023-04-14 14:21:17 +0200

    la til fonts

--- froya/src/App.vue
+++ froya/src/App.vue
@@ -18,10 +18,30 @@ export default {
 </script>
 
 <style>
+
+@font-face {
+  font-family: 'GothemLight';
+  src: url('@/assets/fonts/Gotham-OTF/Gotham-Light.otf') format('otf');
+}
+@font-face {
+  font-family: GothemBook;
+  src: url('@/assets/fonts/Gotham-OTF/Gotham-Book.otf') format('otf');
+}
+
+
 body{
     background-color: #151517;
 }
-h1,h2,h3,h4,h5,p{
+h1,h2,h3,h4,h5{
   color:white;
+  font-family: GothemLight;
+}
+p{
+  color: white;
+  font-family: GothemBook;
+}
+
+.btn{
+  font-family: GothemLight;
 }
 </style>
